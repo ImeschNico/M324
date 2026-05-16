732307c4b9d15ca6a1d63d59eeb7b61fd3a47f8e
nicoi <nico.imesch@outlook.com>
2026-05-16 10:13:30 +0200

    App title geändert

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -107,6 +107,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1>ToDo Liste</h1>
+        <h1>Meine Aufgaben</h1>
         <form onSubmit={handleSubmit} className="todo-form">
           <label htmlFor="taskdescription">Neues Todo anlegen:</label>
           <input type="text" value={taskdescription} onChange={handleChange} />
