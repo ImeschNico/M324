1441ce4f7777acce3190f3b725932233a82ee537
nicoi <nico.imesch@outlook.com>
2026-05-16 10:04:19 +0200

    feat: added prio for tasks

--- backend/src/main/java/com/example/demo/Task.java
+++ backend/src/main/java/com/example/demo/Task.java
@@ -1,22 +1,29 @@
 package com.example.demo;
 
-/** the simplest task 
- * 
- * @author luh
+/**
+ * the simplest task
  */
 public class Task {
-	
-	private String taskdescription; // must have the EXACT name as his React state property and may not be ignored!
 
-	public Task() {
+    private String taskdescription;
+    private String priority = "Mittel";
+
+    public Task() {
     }
 
-	public String getTaskdescription() { // do not apply camel-case here! Its a Bean!
-		return taskdescription;
-	}
+    public String getTaskdescription() {
+        return taskdescription;
+    }
 
-	public void setTaskdescription(String taskdescription) { // do not apply camel-case here! Its a Bean!
-		this.taskdescription = taskdescription;
-	}
+    public void setTaskdescription(String taskdescription) {
+        this.taskdescription = taskdescription;
+    }
 
+    public String getPriority() {
+        return priority;
+    }
+
+    public void setPriority(String priority) {
+        this.priority = priority;
+    }
 }
